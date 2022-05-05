2bef35efa0fd1c58efd705248f6c6d37ad1df001
Aatman <aatman@auroville.org.in>
2022-05-05 17:00:13 +0530

    feat!: upgrade cdk8s to v2 (#3)
    * feat: upgrade cdk8s to v2
    
    * chore: remove deps upgrade

--- .projenrc.js
+++ .projenrc.js
@@ -1,26 +1,21 @@
-const { ConstructLibraryCdk8s, NpmAccess } = require('projen');
+const { ConstructLibraryCdk8s } = require('projen/lib/cdk8s');
+const { NpmAccess } = require('projen/lib/javascript');
 const project = new ConstructLibraryCdk8s({
   author: 'Gagan Singh',
   authorAddress: 'gaganpreet.singh@smallcase.com',
-  cdk8sVersion: '1.0.0-beta.21',
-  cdk8sPlusVersion: '1.0.0-beta.24',
-  constructsVersion: '3.3.80',
+  cdk8sVersion: '2.2.74',
+  constructsVersion: '10.0.5',
   defaultReleaseBranch: 'main',
   name: '@opencdk8s/cdk8s-sealed-secrets-controller',
   repositoryUrl: 'https://github.com/opencdk8s/cdk8s-sealed-secrets-controller.git',
-  npmAccess: 'public',
-  mergify: true,
-
   npmAccess: NpmAccess.PUBLIC,
+  mergify: true,
   python: {
     distName: 'cdk8s-sealed-secrets-controller',
     module: 'cdk8s_sealed_secrets_controller',
   },
 
-
-  peerDeps: [
-    'constructs@^3.3.80',
-  ],
+  depsUpgrade: false,
   // deps: [],                          /* Runtime dependencies of this module. */
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                       /* Build dependencies for this module. */
