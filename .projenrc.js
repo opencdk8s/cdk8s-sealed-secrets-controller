9c5235b57aa64d4822a525b99a46fbe54ae533da
Hunter-Thompson <aatman@auroville.org.in>
2022-05-05 16:52:39 +0530

    chore: remove deps upgrade

--- .projenrc.js
+++ .projenrc.js
@@ -15,6 +15,7 @@ const project = new ConstructLibraryCdk8s({
     module: 'cdk8s_sealed_secrets_controller',
   },
 
+  depsUpgrade: false,
   // deps: [],                          /* Runtime dependencies of this module. */
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                       /* Build dependencies for this module. */
